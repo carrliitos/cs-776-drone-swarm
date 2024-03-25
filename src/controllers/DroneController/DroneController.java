66139b4ddbeec04e8f30bf90cc6cbdd5b2332c0a
carrliitos <salazar.ten80@gmail.com>
2024-03-24 19:50:05 -0500

    empirically found acceptable vertical, roll, and pitch constants

--- src/controllers/DroneController/DroneController.java
+++ src/controllers/DroneController/DroneController.java
@@ -20,9 +20,9 @@ public class DroneController extends Robot {
   // Constants
   private static final double K_VERTICAL_THRUST = 68.5; // with this thrust, the drone lifts.
   private static final double K_VERTICAL_OFFSET = 0.6; // Vertical offset where the robot actually targets to stabilize itself.
-  private static final double K_VERTICAL_P = 3.0; // P constant of the vertical PID.
-  private static final double K_ROLL_P = 50.0; // P constant of the roll PID.
-  private static final double K_PITCH_P = 30.0; // P constant of the pitch PID.
+  private static final double K_VERTICAL_P = 2.0; // P constant of the vertical PID.
+  private static final double K_ROLL_P = 10.0; // P constant of the roll PID.
+  private static final double K_PITCH_P = 10.0; // P constant of the pitch PID.
   
   public DroneController() {
     super();
@@ -60,8 +60,10 @@ public class DroneController extends Robot {
   
   private void displayWelcomeMessage() {
     System.out.println("Starting the drone...");
+    // Wait one second
+    double previousTime = 0.0;
     while (step(TIME_STEP) != -1) {
-      if (getTime() > 1.0) { break; }
+      if (getTime() - previousTime > 1.0) { break; }
     }
   }
   
