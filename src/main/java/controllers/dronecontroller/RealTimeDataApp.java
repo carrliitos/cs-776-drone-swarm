7ae0c989f0991be47d953f1880be4cef8648f008
carrliitos <salazar.ten80@gmail.com>
2024-04-18 05:04:42 -0500

    visualize just the last maxDataPoints datapoints

--- src/main/java/controllers/dronecontroller/RealTimeDataApp.java
+++ src/main/java/controllers/dronecontroller/RealTimeDataApp.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class RealTimeDataApp {
   private static final String X_AXIS_LABEL = "Time";
-  private static final String Y_AXIS_LABEL = "Value";
+  private static final String Y_AXIS_LABEL = "Signals";
   private static final int WIDTH = 1000;
   private static final int HEIGHT = 600;
   private List<Double> xData = new ArrayList<>();
   private List<List<Double>> yDataList = new ArrayList<>();
-  private int maxDataPoints = 1000; // Maximum number of data points to display
+  private int maxDataPoints = 100; // Maximum number of data points to display
   private int currentDataIndex = 0;
   private double Y_MIN = -10;
   private double Y_MAX = 10;
@@ -43,14 +43,11 @@ public class RealTimeDataApp {
   private void plotData(Graphics g, String[] labels) {
     if (!xData.isEmpty()) {
       Graphics2D g2d = (Graphics2D) g.create();
-
-      // Initialize min and max values
       double xMin = xData.get(0);
       double xMax = xData.get(xData.size() - 1);
       double yMin = Y_MIN;
       double yMax = Y_MAX;
 
-      // Find min and max values in yDataList
       for (List<Double> dataList : yDataList) {
         if (!dataList.isEmpty()) {
           yMin = Math.min(yMin, dataList.stream().min(Double::compare).orElse(0.0));
@@ -60,7 +57,6 @@ public class RealTimeDataApp {
       double xScale = WIDTH / (xMax - xMin);
       double yScale = HEIGHT / (yMax - yMin);
 
-      // Plot the data points
       Color[] colors = {Color.BLUE, Color.RED, Color.GREEN, Color.ORANGE};
       for (int i = 0; i < 4; i++) {
         g2d.setColor(colors[i]);
@@ -71,7 +67,6 @@ public class RealTimeDataApp {
           int y2 = HEIGHT - (int) ((yDataList.get(i).get(j) - yMin) * yScale);
           g2d.drawLine(x1, y1, x2, y2);
         }
-        // Draw labels
         g2d.setColor(colors[i]);
         g2d.drawString(labels[i], 10, 20 + i * 20);
       }
@@ -82,11 +77,12 @@ public class RealTimeDataApp {
       g2d.drawString(Y_AXIS_LABEL, -HEIGHT / 2 - 20, 10);
       g2d.rotate(Math.PI / 2);
 
-      // Draw x tick marks
+      // Draw x tick marks and labels
       g2d.setColor(Color.GRAY);
-      for (double xTick = xMin; xTick <= xMax; xTick++) {
+      for (double xTick = Math.ceil(xMin / 5) * 5; xTick <= xMax; xTick += 5) {
         int x = (int) ((xTick - xMin) * xScale);
         g2d.drawLine(x, HEIGHT - 5, x, HEIGHT + 5);
+        g2d.drawString(String.format("%.0f", xTick), x - 10, HEIGHT + 20);
       }
 
       // Draw y tick marks and labels
@@ -104,7 +100,6 @@ public class RealTimeDataApp {
 
       g2d.dispose();
     } else {
-      // Display a message if no data is available
       Graphics2D g2d = (Graphics2D) g.create();
       g2d.setColor(Color.BLACK);
       g2d.drawString("No data available", 10, 20);
@@ -115,11 +110,16 @@ public class RealTimeDataApp {
   public void visualize(double[] yValues) {
     double x = currentX++;
     xData.add(x);
+
+    if (xData.size() > maxDataPoints) {
+      xData.remove(0);
+      for (List<Double> dataList : yDataList) {
+        dataList.remove(0);
+      }
+    }
+
     for (int i = 0; i < 4; i++) {
       yDataList.get(i).add(Math.min(Math.max(yValues[i], Y_MIN), Y_MAX));
-      if (yDataList.get(i).size() > maxDataPoints) {
-        yDataList.get(i).remove(0);
-      }
     }
 
     chartPanel.repaint();
